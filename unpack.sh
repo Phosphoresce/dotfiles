4bc3eb3d8226724636d7f580cdd1de76744f38de
phosphoresce <phosphoresce.k@gmail.com>
2017-01-30 08:21:55 -0600

    smarter copy

--- unpack.sh
+++ unpack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Unpack dotfiles scripts docs
-cp -rnv ./home/.* ~
-cp -nv ./docs/* ~/Documents
-cp -nv ./scripts/* ~/Scripts
+cp -riuv ./home/.* ~
+cp -iuv ./docs/* ~/Documents
+cp -iuv ./scripts/* ~/Scripts

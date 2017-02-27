885884a3ee0a0280ec2412885cfacd1555b240ea
phosphoresce <phosphoresce.k@gmail.com>
2017-02-27 11:12:48 -0600

    rm redundant vim colors updated unpack script

--- unpack.sh
+++ unpack.sh
@@ -3,3 +3,6 @@
 cp -ruv ./home/.* ~
 cp -uv ./docs/* ~/Documents
 cp -uv ./scripts/* ~/Scripts
+cp -uv ./awesome/rc.lua ~/.config/awesome/rc.lua
+cp -uv ./awesome/arch_small.png /usr/share/awesome/icons
+sudo cp -ruv ./awesome/dark /usr/share/awesome/themes

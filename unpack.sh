f69fe01429cdad3bbdc3a494ba5d9a84177aece9
phosphoresce <phosphoresce.k@gmail.com>
2017-03-22 09:20:21 -0500

    made the unpack script bash friendly...

--- unpack.sh
+++ unpack.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 # Unpack dotfiles scripts docs
-cp -ruv ./home/.* ~
-cp -uv ./docs/* ~/Documents
-cp -uv ./scripts/* ~/Scripts
-cp -uv ./awesome/rc.lua ~/.config/awesome/rc.lua
-cp -uv ./awesome/arch_small.png /usr/share/awesome/icons
-sudo cp -ruv ./awesome/dark /usr/share/awesome/themes
+rsync -ruv home/ ~
+rsync -ruv docs/ ~/Documents
+rsync -ruv scripts/ ~/Scripts
+sudo rsync -ruv awesome/dark /usr/share/awesome/themes
+cp -uv awesome/rc.lua ~/.config/awesome/rc.lua
+cp -uv awesome/arch_small.png /usr/share/awesome/icons

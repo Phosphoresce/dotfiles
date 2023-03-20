3e41cd272d19082ce5f855043ffa57521534ef99
klepert <phosphoric@protonmail.com>
2023-03-20 12:25:24 -0400

    update vim setup

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -3,3 +3,7 @@
 #Install Vundle and plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+mkdir -p ~/.config/nvim
+ln -s ~/.vim ~/.local/share/nvim/site
+ln -s ~/.vimrc ~/.config/nvim/init.vim
